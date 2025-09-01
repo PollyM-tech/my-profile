4d8f282fa1d0510fa579d3e6946b815d7f0cbec2
Pauline Moraa <pmoraa431@gmail.com>
2025-09-01 13:08:48 +0300

    added dist to config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/", 
   build: {
     outDir: "dist",
-  },
-  server: {
-    host: true, 
-    port: process.env.PORT || 3000, 
   },
 });
